7c0ba59a76923cbac7d16618d356333262d0c2df
Zehrasenturk1828 <Zehrasenturk778@gmail.com>
2024-04-02 00:42:37 +0300

    update

---
+++ Constants/globalConstants.py
@@ -0,0 +1,35 @@
+BASE_URL = "https://www.saucedemo.com/"
+username_id = "user-name"
+password_id = "password"
+login_button_id = "login-button"
+
+invalid_errorMessage_xpath = "//*[@id='login_button_container']/div/form/div[3]/h3"
+invalid_errorMessage_text = "Epic sadface: Username and password do not match any user in this service"
+
+baslik_xpath = "//*[@id='header_container']/div[1]/div[2]/div"
+baslik_text = "Swag Labs"
+
+empty_errorMessage_xpath ="//*[@id='login_button_container']/div/form/div[3]/h3"
+empty_errorMessage_text = "Epic sadface: Username is required"
+
+empty_username = ""
+empty_password = ""
+emptyPassword_errorMessage_xpath = "//*[@id='login_button_container']/div/form/div[3]/h3"
+emptyPassword_errorMessage_text = "Epic sadface: Password is required"
+
+locked_username = "locked_out_user"
+locked_password = "secret_sauce"
+locked_errorMessage_xpath = "//*[@id='login_button_container']/div/form/div[3]/h3"
+locked_errorMessage_text = "Epic sadface: Sorry, this user has been locked out."
+
+valid_username = "standard_user"
+valid_password = "secret_sauce"
+
+addToCard_id = "add-to-cart-sauce-labs-bike-light"
+basket_piece_cssSelector = "span.shopping_cart_badge"
+
+remove_id = "remove-sauce-labs-bike-light"
+
+product_xpath = "//*[@id='item_4_title_link']/div"
+product_name = "Sauce Labs Backpack"
+productDetails_name_xpath = "//*[@id='inventory_item_container']/div/div/div[2]/div[1]"
--- test_saucedemo.py
+++ test_saucedemo.py
@@ -5,6 +5,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.action_chains import ActionChains
+import openpyxl
+from Constants.globalConstants import *
 
 class Test_Sauce:   
      
@@ -13,150 +15,191 @@ class Test_Sauce:
      def setup_method(self):
         self.driver = webdriver.Chrome(ChromeDriverManager().install())
         self.driver.maximize_window()
-        self.driver.get("https://www.saucedemo.com/")
+        self.driver.get(BASE_URL)
 
      #her test bitiminde çalışacak fonksiyon
      def teardown_method(self):
         self.driver.quit()
 
-     
-     @pytest.mark.parametrize("username,password",[("1","1"),("abc","123"),("deneme","secret_sauce")])
+     def readInvalidDataFromExcel():
+         excelFile = openpyxl.load_workbook("data/invalidLogin.xlsx")
+         sheet = excelFile["Sheet1"]
+         rows = sheet.max_row #kaçıncı satıra kadar benim verim var
+         data = []
+         for i in range(2,rows+1):
+             username = sheet.cell(i,1).value
+             password = sheet.cell(i,2).value
+             data.append((username,password))
+         return data
+             
+             
+     @pytest.mark.parametrize("username,password",readInvalidDataFromExcel())
      def test_invalid_login(self,username,password):
-        userNameInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"user-name")))
-        passwordInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"password")))
+        #userNameInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,username_id)))
+        self.waitForElementVisible((By.ID,username_id))
+        userNameInput = self.driver.find_element(By.ID,username_id)
+        #passwordInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,password_id)))
+        self.waitForElementVisible((By.ID,password_id))
+        passwordInput = self.driver.find_element(By.ID,password_id)
         userNameInput.send_keys(username)
         passwordInput.send_keys(password)
-        loginButton = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"login-button")))
+        #loginButton = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,login_button_id)))
+        self.waitForElementVisible((By.ID,login_button_id))
+        loginButton = self.driver.find_element(By.ID,login_button_id)
         loginButton.click()
-        errorMessage =WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.XPATH,"//*[@id='login_button_container']/div/form/div[3]/h3")))
-        assert errorMessage.text == "Epic sadface: Username and password do not match any user in this service"
+        #errorMessage =WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.XPATH,errorMessage_xpath)))
+        self.waitForElementVisible((By.XPATH,invalid_errorMessage_xpath))
+        errorMessage = self.driver.find_element(By.XPATH,invalid_errorMessage_xpath)
+        assert errorMessage.text == invalid_errorMessage_text
 
      def test_empty_username_password(self):
          self.driver.get("https://www.saucedemo.com./")
-         userNameInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"user-name")))
-         passwordInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"password")))
+         #userNameInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,username_id)))
+         self.waitForElementVisible((By.ID,username_id))
+         userNameInput = self.driver.find_element(By.ID,username_id)
+         #passwordInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,password_id)))
+         self.waitForElementVisible((By.ID,password_id))
+         passwordInput = self.driver.find_element(By.ID,password_id)
          actions = ActionChains(self.driver)
-         actions.send_keys_to_element(userNameInput,"")
-         actions.send_keys_to_element(passwordInput,"")
+         actions.send_keys_to_element(userNameInput,empty_username)
+         actions.send_keys_to_element(passwordInput,empty_password)
          actions.perform() #depoladığım aksiyonları çalıştır
-         loginButton = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"login-button")))
+         self.waitForElementVisible((By.ID,login_button_id))
+         loginButton = self.driver.find_element(By.ID,login_button_id)
          loginButton.click()
-         errorMessage = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.XPATH,"//*[@id='login_button_container']/div/form/div[3]/h3")))
-         print(errorMessage.text)
-         testResult = errorMessage.text == "Epic sadface: Username is required"
-         print(f"TEST SONUCU: {testResult}")
+         self.waitForElementVisible((By.XPATH,empty_errorMessage_xpath))
+         errorMessage = self.driver.find_element(By.XPATH,empty_errorMessage_xpath)
+         assert errorMessage.text == empty_errorMessage_text
 
      def test_empty_password(self):
          self.driver.get("https://www.saucedemo.com./")
-         userNameInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"user-name")))
-         passwordInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"password")))
+         #userNameInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,username_id)))
+         self.waitForElementVisible((By.ID,username_id))
+         userNameInput = self.driver.find_element(By.ID,username_id)
+         #passwordInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,password_id)))
+         self.waitForElementVisible((By.ID,password_id))
+         passwordInput = self.driver.find_element(By.ID,password_id)
          actions = ActionChains(self.driver)
          actions.send_keys_to_element(userNameInput,"1")
-         actions.send_keys_to_element(passwordInput,"")
+         actions.send_keys_to_element(passwordInput,empty_password)
          actions.perform()
-         loginButton = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"login-button")))
+         self.waitForElementVisible((By.ID,login_button_id))
+         loginButton = self.driver.find_element(By.ID,login_button_id)
          loginButton.click()
-         errorMessage = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.XPATH,"//*[@id='login_button_container']/div/form/div[3]/h3")))
-         print(errorMessage.text)
-         testResult = errorMessage.text == "Epic sadface: Password is required"
-         print(f"TEST SONUCU: {testResult}")
+         self.waitForElementVisible((By.XPATH,emptyPassword_errorMessage_xpath))
+         errorMessage = self.driver.find_element(By.XPATH,emptyPassword_errorMessage_xpath)
+         assert errorMessage.text == emptyPassword_errorMessage_text
     
      def test_user_locked_out(self):
          self.driver.get("https://www.saucedemo.com./")
-         userNameInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"user-name")))
-         passwordInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"password")))
+         #userNameInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,username_id)))
+         self.waitForElementVisible((By.ID,username_id))
+         userNameInput = self.driver.find_element(By.ID,username_id)
+         #passwordInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,password_id)))
+         self.waitForElementVisible((By.ID,password_id))
+         passwordInput = self.driver.find_element(By.ID,password_id)
          actions = ActionChains(self.driver)
-         actions.send_keys_to_element(userNameInput,"locked_out_user")
-         actions.send_keys_to_element(passwordInput,"secret_sauce")
+         actions.send_keys_to_element(userNameInput,locked_username)
+         actions.send_keys_to_element(passwordInput,locked_password)
          actions.perform()
-         loginButton = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"login-button")))
+         self.waitForElementVisible((By.ID,login_button_id))
+         loginButton = self.driver.find_element(By.ID,login_button_id)
          loginButton.click()
-         errorMessage = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.XPATH,"//*[@id='login_button_container']/div/form/div[3]/h3")))
-         print(errorMessage.text)
-         testResult = errorMessage.text == "Epic sadface: Sorry, this user has been locked out."
-         print(f"TEST SONUCU: {testResult}")
+         self.waitForElementVisible((By.XPATH,locked_errorMessage_xpath))
+         errorMessage = self.driver.find_element(By.XPATH,locked_errorMessage_xpath)
+         assert errorMessage.text == locked_errorMessage_text
 
      def test_valid_login(self):
          self.driver.get("https://www.saucedemo.com./")
-         userNameInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"user-name")))
-         passwordInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"password")))
+         #userNameInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,username_id)))
+         self.waitForElementVisible((By.ID,username_id))
+         userNameInput = self.driver.find_element(By.ID,username_id)
+         #passwordInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,password_id)))
+         self.waitForElementVisible((By.ID,password_id))
+         passwordInput = self.driver.find_element(By.ID,password_id)
          actions = ActionChains(self.driver)
-         actions.send_keys_to_element(userNameInput,"standard_user")
-         actions.send_keys_to_element(passwordInput,"secret_sauce")
+         actions.send_keys_to_element(userNameInput,valid_username)
+         actions.send_keys_to_element(passwordInput,valid_password)
          actions.perform()
-         loginButton = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"login-button")))
+         self.waitForElementVisible((By.ID,login_button_id))
+         loginButton = self.driver.find_element(By.ID,login_button_id)
          loginButton.click()
-         baslik = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.XPATH,"//*[@id='header_container']/div[1]/div[2]/div")))
-         assert baslik.text == "Swag Labs"
+         baslik = self.driver.find_element(By.XPATH,baslik_xpath)
+         assert baslik.text == baslik_text
 
-     def add_to_basket(self):
+     def test_add_to_basket(self):
          self.driver.get("https://www.saucedemo.com./")
-         userNameInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"user-name")))
-         passwordInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"password")))
+         #userNameInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,username_id)))
+         self.waitForElementVisible((By.ID,username_id))
+         userNameInput = self.driver.find_element(By.ID,username_id)
+         #passwordInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,password_id)))
+         self.waitForElementVisible((By.ID,password_id))
+         passwordInput = self.driver.find_element(By.ID,password_id)
          actions = ActionChains(self.driver)
-         actions.send_keys_to_element(userNameInput,"standard_user")
-         actions.send_keys_to_element(passwordInput,"secret_sauce")
+         actions.send_keys_to_element(userNameInput,valid_username)
+         actions.send_keys_to_element(passwordInput,valid_password)
          actions.perform()
-         loginButton = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"login-button")))
+         self.waitForElementVisible((By.ID,login_button_id))
+         loginButton = self.driver.find_element(By.ID,login_button_id)
          loginButton.click()
-         addToCard = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"add-to-cart-sauce-labs-bike-light"))) 
+         self.waitForElementVisible((By.ID,addToCard_id))
+         addToCard = self.driver.find_element(By.ID,addToCard_id)
          addToCard.click()
-         basket_piece = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.CSS_SELECTOR,"span.shopping_cart_badge")))
+         self.waitForElementVisible((By.CSS_SELECTOR,basket_piece_cssSelector))
+         basket_piece = self.driver.find_element(By.CSS_SELECTOR,basket_piece_cssSelector)
          basket_piece.text > "0"
          print(f"Ürün başarıyla sepete eklendi")
 
-     def remove_to_basket(self):
+     def test_remove_to_basket(self):
          self.driver.get("https://www.saucedemo.com./")
-         userNameInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"user-name")))
-         passwordInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"password")))
+         #userNameInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,username_id)))
+         self.waitForElementVisible((By.ID,username_id))
+         userNameInput = self.driver.find_element(By.ID,username_id)
+         #passwordInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,password_id)))
+         self.waitForElementVisible((By.ID,password_id))
+         passwordInput = self.driver.find_element(By.ID,password_id)
          actions = ActionChains(self.driver)
-         actions.send_keys_to_element(userNameInput,"standard_user")
-         actions.send_keys_to_element(passwordInput,"secret_sauce")
+         actions.send_keys_to_element(userNameInput,valid_username)
+         actions.send_keys_to_element(passwordInput,valid_password)
          actions.perform()
-         loginButton = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"login-button")))
+         self.waitForElementVisible((By.ID,login_button_id))
+         loginButton = self.driver.find_element(By.ID,login_button_id)
          loginButton.click()
-         addToCard = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"add-to-cart-sauce-labs-bike-light"))) 
+         self.waitForElementVisible((By.ID,addToCard_id))
+         addToCard = self.driver.find_element(By.ID,addToCard_id)
          addToCard.click()
-         basket_piece = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.CSS_SELECTOR,"span.shopping_cart_badge")))
+         self.waitForElementVisible((By.CSS_SELECTOR,basket_piece_cssSelector))
+         basket_piece = self.driver.find_element(By.CSS_SELECTOR,basket_piece_cssSelector)
          basket_piece.text > "0"
          print(f"Ürün başarıyla sepete eklendi, sepetteki ürün sayısı: {basket_piece.text}")
-         remove = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"remove-sauce-labs-bike-light"))) 
+         self.waitForElementVisible((By.ID,remove_id))
+         remove = self.driver.find_element(By.ID,remove_id)
          remove.click()
          print(f"Ürün başarıyla sepetten çıkarıldı.")
 
-     def product_details_page(self):
+     def test_product_details_page(self):
          self.driver.get("https://www.saucedemo.com./")
-         userNameInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"user-name")))
-         passwordInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"password")))
+         #userNameInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,username_id)))
+         self.waitForElementVisible((By.ID,username_id))
+         userNameInput = self.driver.find_element(By.ID,username_id)
+         #passwordInput = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,password_id)))
+         self.waitForElementVisible((By.ID,password_id))
+         passwordInput = self.driver.find_element(By.ID,password_id)
          actions = ActionChains(self.driver)
-         actions.send_keys_to_element(userNameInput,"standard_user")
-         actions.send_keys_to_element(passwordInput,"secret_sauce")
+         actions.send_keys_to_element(userNameInput,valid_username)
+         actions.send_keys_to_element(passwordInput,valid_password)
          actions.perform()
-         loginButton = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.ID,"login-button")))
+         self.waitForElementVisible((By.ID,login_button_id))
+         loginButton = self.driver.find_element(By.ID,login_button_id)
          loginButton.click()
-         product = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.XPATH,"//*[@id='item_4_title_link']/div")))
+         self.waitForElementVisible((By.XPATH,product_xpath))
+         product = self.driver.find_element(By.XPATH,product_xpath)
          product.click()
-         product_name = "Sauce Labs Backpack"
-         product_details_name = WebDriverWait(self.driver,5).until(ec.visibility_of_element_located((By.XPATH,"//*[@id='inventory_item_container']/div/div/div[2]/div[1]")))
-         product_details_name.text == product_name
-         print(f"Erişilen ürün detay sayfasının adı: {product_details_name.text}")  
+         self.waitForElementVisible((By.XPATH,productDetails_name_xpath))
+         product_details_name = self.driver.find_element(By.XPATH,productDetails_name_xpath)
+         assert product_details_name.text == product_name
 
 
+     def waitForElementVisible(self,locator,timeout=5):
+        return WebDriverWait(self.driver,timeout).until(ec.visibility_of_element_located(locator))
 
-
-         
-         
-         
-         
-
-        
-
-
-
-#testClass = Test_Sauce()
-#testClass.setup_method()
-#testClass.test_invalid_login()
-#testClass.teardown_method()
-#testClass.test_empty_password()
-#testClass.test_user_locked_out()
-#testClass.test_valid_login()
